717a4df996679cc0486830bf151d62fd9eddf15f
orzino3 <155395782+orzino3@users.noreply.github.com>
2024-07-28 22:38:35 +0300

    Update Titanic.java

--- Titanic.java
+++ Titanic.java
@@ -1,37 +1,18 @@
-public class Constants {
-    public static final String PATH_TO_DATA_FILE = "src/data/titanic.csv";
-    public static final int WINDOW_WIDTH = 600;
-    public static final int WINDOW_HEIGHT = 800;
-    public static final String[] PASSENGER_CLASS_OPTIONS = { "All", "1st", "2nd", "3rd" };
-    public static final int MARGIN_FROM_TOP = 10;
-    public static final int MARGIN_FROM_LEFT = 5;
-    public static final int LABEL_WIDTH = 150;
-    public static final int LABEL_HEIGHT = 30;
-    public static final int COMBO_BOX_WIDTH = 80;
-    public static final int COMBO_BOX_HEIGHT = 30;
-    public static final int PASSENGER_ID_INDEX = 0;
-    public static final int SURVIVED_INDEX = 1;
-    public static final int P_CLASS_INDEX = 2;
-    public static final int NAME_FIRST_PART_INDEX = 3;
-    public static final int NAME_SECOND_PART_INDEX = 4;
-    public static final int SEX_INDEX = 5;
-    public static final int AGE_INDEX = 6;
-    public static final int SIB_SP_INDEX = 7;
-    public static final int PARCH_INDEX = 8;
-    public static final int TICKET_ID_INDEX = 9;
-    public static final int FARE_INDEX = 10;
-    public static final int CABIN_INDEX = 11;
-    public static final int EMBARKED_INDEX = 12;
-    public static final int OBJECT_MARGIN = 50;
-    public static final int GAP = 15;
-    public static final double HUNDRED = 100.0;
-    public static final int CHILD = 17;
-    public static final int ADULT_MIN = 18;
-    public static final int ADULT_MAX = 64;
-    public static final int ELDER = 65;
-    public static final double LOW_FARE = 49.0;
-    public static final double MID_FARE_MIN = 50.0;
-    public static final double MID_FARE_MAX = 99.0;
-    public static final double HIGH_FARE = 100.0;
-}
+import javax.swing.*;
 
+class Titanic extends JFrame {
+
+    public static void main(String[] args) {
+        new Titanic();
+    }
+
+    public Titanic() {
+        this.setTitle("Titanic Passengers Data");
+        this.setLayout(null);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+        this.add(new ManageScreen(0, 0, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+        this.setVisible(true);
+
+    }
+}
